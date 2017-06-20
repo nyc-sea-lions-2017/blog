f3bd7bb1fddb39a8c0274b01a445f5c7262de4fc
Carlo Francisco <cfrancisco726@gmail.com>
2017-06-19 20:10:31 -0400

    done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "carlo", password: "carlo", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
